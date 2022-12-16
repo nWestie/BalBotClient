046ac846bd6f6491822d0cf8294a561a196efae0
Noah West <westnoahb@gmail.com>
2022-12-15 19:51:52 -0500

    improve bounding w/ pong demo

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
-from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty
+from kivy.properties import NumericProperty, ReferenceListProperty, ObjectProperty, BoundedNumericProperty
 from kivy.vector import Vector
 from kivy.clock import Clock
 from random import randint
@@ -16,7 +16,6 @@ class PongBall(Widget):
 
 
 class PongPlayer(Widget):
-
     score = NumericProperty(0)
 
     def bounceBall(self, ball):
@@ -29,30 +28,42 @@ class PongPlayer(Widget):
 
 
 class PongGame(Widget):
-    ball = ObjectProperty(None)
-    plr1 = ObjectProperty(None)
-    plr2 = ObjectProperty(None)
+    ball = ObjectProperty()
+    plr1 = ObjectProperty()
+    plr2 = ObjectProperty()
 
     def update(self, dt):
         self.ball.move()
 
         self.plr1.bounceBall(self.ball)
         self.plr2.bounceBall(self.ball)
-        
+
         if (self.ball.y < 0) or (self.ball.top > self.height):
             self.ball.velY *= -1
-        if (self.ball.x < 0) or (self.ball.right > self.width):
-            self.ball.velX *= -1
+            self.ball.vel = Vector(self.ball.velX, self.ball.velY)
+        if (self.ball.x < 0):
+            self.plr2.score *= 2
+            self.serveBall((4, 0))
+        if (self.ball.right > self.width):
+            self.plr1.score += 1
+            self.serveBall((-4, 0))
 
-    def serveBall(self):
+    def serveBall(self, vel=(4, 0)):
         self.ball.center = self.center
-        self.ball.vel = Vector(4, 0).rotate(45)
+        self.ball.vel = vel
 
     def on_touch_move(self, touch):
+        y = touch.y
+        h2 = self.plr1.height / 2
+        if (y + h2) > self.height:
+            y = self.height-h2
+        if (y - h2) < 0:
+            y = 0+h2
+
         if touch.x < self.width/3:
-            self.plr1.center_y = touch.y
+            self.plr1.center_y = y
         if touch.x > self.width * 2/3:
-            self.plr2.center_y = touch.y
+            self.plr2.center_y = y
 
 
 class PongApp(App):
