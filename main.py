7cb79d338836da3a39ffe51ed26bf626ddc996f5
Noah West <westnoahb@gmail.com>
2022-12-30 23:12:59 -0500

    prototype coming together
    got PID and trim spinners roughly done, just need to hookup to BT
    slider is causing trouble, cannot get it to return to center when done.

--- main.py
+++ main.py
@@ -1,23 +1,51 @@
 from kivymd.app import MDApp
 from kivymd.uix.widget import MDWidget
-from kivymd.uix.button import MDFlatButton
+from kivymd.uix.button import MDFlatButton, MDRectangleFlatButton
 from kivymd.uix.behaviors.toggle_behavior import MDToggleButton
+from kivy.properties import ObjectProperty, NumericProperty, StringProperty
+from kivymd.uix.boxlayout import MDBoxLayout
+from kivy.uix.slider import Slider
 
 
 class LoginScreen(MDWidget):
     pass
+class SprungSlider(Slider):
+    sVal = NumericProperty(None)
 
+    def sliderReleased(self):
+        print(self.sVal)
+        self.sVal +=10
 
-class MyToggle(MDFlatButton, MDToggleButton):
-    pass
+    
+pidStep = 1
+class PIDButton(MDRectangleFlatButton, MDToggleButton):
+    def pressed(self, buttonText: str):
+        global pidStep
+        pidStep = float(buttonText)
+
 
 
 class MainApp(MDApp):
     def build(self):
         self.theme_cls.theme_style = "Dark"
-        self.theme_cls.primary_palette = "Orange"
-        self.theme_cls.theme_style_switch_animation = True
+        self.theme_cls.primary_palette = "Purple"
+        self.theme_cls.primary_hue = '600'
+        self.theme_cls.primary_dark_hue = '900'
+        self.theme_cls.primary_light_hue = '400'
+        self.theme_cls.accent_palette = "Orange"
         return LoginScreen()
+
+class NumSpinner(MDBoxLayout):
+    value = NumericProperty(None)
+
+    def plusPressed(self):
+        self.value += pidStep
+
+    def minusPressed(self):
+        self.value -= pidStep
+
+    def textUpdated(self, str):
+        self.value = max(float(str),0)
 
 
 if __name__ == "__main__":
