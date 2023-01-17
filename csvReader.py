9d6e8991766adac423f0cd76019c2c9902d5dd61
Noah West <westnoahb@gmail.com>
2023-01-16 22:17:44 -0500

    update plotter

--- csvReader.py
+++ csvReader.py
@@ -2,9 +2,10 @@ from plyer import filechooser
 import numpy as np
 from matplotlib import pyplot as plt
 
-plt.style.use('dark_background')
+# plt.style.use('dark_background')
 
-fileName = ''#C:/Users/WESTNB21/Documents/Code/BalBotClient/logData/01-13-23/19.18.02-data.csv"
+# fileName = ''
+fileName = "C:/Users/WESTNB21/Documents/Code/BalBotClient/logData/01-13-23/19.18.02-data.csv"
 
 if not fileName:
     fileName = filechooser.open_file(
@@ -15,7 +16,6 @@ if not fileName:
     )[0]
 print('Opened: ')
 print(fileName, '\n')
-
 b = np.loadtxt(fileName, dtype=np.float32, delimiter=',', usecols=(0,2,3))
 plt.plot(b[:,0],b[:,1])
 plt.plot(b[:,0],b[:,2])
